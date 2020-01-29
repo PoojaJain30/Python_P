21e2e2818d15fb0cb22e68caa8de137ddeb49ac1
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-01-29 12:13:23 -0600

    Rev2

--- StringMethods.py
+++ StringMethods.py
@@ -31,3 +31,32 @@ def master_yoda(text):
     return ' '.join(li)
 
 master_yoda('I am home')
+
+####ANIMAL CRACKERS: Write a function takes a two-word string and returns True if both words begin with same letter
+def animal_crackers(text):
+    stri_1 = text.split()[0]
+    stri_2 = text.split()[1]
+    if stri_1[0] == stri_2[0]:
+        return True
+    else:
+        return False
+animal_crackers("my mane")
+
+#better approch
+def animal_crackers_1(text):
+    name = text.split()
+    return name[0][0] == name[1][0]
+
+animal_crackers_1('Levelheaded llama')
+
+####PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
+def paper_doll(text):
+    li=[]
+    for i,ch in enumerate(text):
+        x=0
+        while(x<3):
+            li.append(ch)
+            x = x+1
+    return ''.join(li)
+
+paper_doll('Hello')
