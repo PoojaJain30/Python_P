69d5564b1fe9b77df3f5683b45368e119ec0daa2
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-01-31 23:12:35 -0600

    REV2

--- oops-account.py
+++ oops-account.py
@@ -31,3 +31,23 @@ a = Account('poo',100)
 a.owner
 a.deposite(50)
 a.withdraw(20)
+
+#other approch
+class Account1:
+    def __init__(self,owner,balance=0):
+        self.owner = owner
+        self.balance = balance
+        
+    def __str__(self):
+        return f'Account owner:   {self.owner}\nAccount balance: ${self.balance}'
+        
+    def deposit(self,dep_amt):
+        self.balance += dep_amt
+        print('Deposit Accepted')
+        
+    def withdraw(self,wd_amt):
+        if self.balance >= wd_amt:
+            self.balance -= wd_amt
+            print('Withdrawal Accepted')
+        else:
+            print('Funds Unavailable!')
