4faee268a20cb25f3f43d56bb299ddc39ccb1b74
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-04-04 20:05:10 -0500

    Rev1

---
+++ Gsheet.py
@@ -0,0 +1,22 @@
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_sheet.json', scope)
+
+gc = gspread.authorize(credentials)
+
+sheet = gc.open('Practice').sheet1
+
+#json_key = json.load(open('creds.json')) # json credentials you downloaded earlier
+#scope = ['https://spreadsheets.google.com/feeds']
+
+#credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope) # get email and key from creds
+
+#file = gspread.authorize(credentials) # authenticate with Google
+#sheet = file.open("Copy_of_Kharcha_US").sheet1
+all_cells = sheet.get_all_records(sheet)
+print(all_cells)
