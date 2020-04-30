7c6a5b9a263010ea5d4dbfc9396e0c146b2d7650
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-04-29 21:43:07 -0500

    Init

--- WebScrapping/scrap-mSchool.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = 'https://www.learningcaregroup.com/about-us/weekly-activities/'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')   
