f422552d6805153223fb8d416385efad93a0e672
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-05-02 10:58:17 -0500

    REV2

--- WebScrapping/scrap-mSchool.py
+++ WebScrapping/scrap-mSchool.py
@@ -1,8 +1,25 @@
+# Get data from the url using beutifulSoup and request
 import requests
 from bs4 import BeautifulSoup
-print("file execss")
-URL = 'https://www.learningcaregroup.com/about-us/weekly-activities/'
-page = requests.get(URL)
+from requests.exceptions import HTTPError
 
-soup = BeautifulSoup(page.content, 'html.parser')  
-print(soup) 
+URL = 'https://www.learningcaregroup.com/about-us/weekly-activities/'
+
+
+try:
+    response = requests.get(URL)
+except HTTPError as err:
+    print(f'Http error occured: {err}')
+except Exception as otherErr:
+    print(f'Other error occured: "{otherErr}')
+else:
+    print('success')
+
+html = BeautifulSoup(response.content, 'html.parser')  
+
+heading = html.div.findAll('a')
+print(heading)
+
+for link in heading:
+    print(link.get('href'))
+    print(link.text)
